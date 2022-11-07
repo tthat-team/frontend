c801764ce30a0b356263c83c33125bc674e95b30
liya-zhu <julia.liya.zhu@gmail.com>
2022-11-06 21:52:32 -0500

    stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {Component, useState} from 'react';
+import React from 'react';
 
 function App() {
 
@@ -10,35 +11,35 @@ function App() {
   function activateLasers(){
     fetch("http:localhost:8080/transactions")
       .then(res => res.json()) //synchronization
-      .then(json => {setTrasactions(json);})
+      .then(json => {setTrasactions(json);}) //not defined
+  }
+
+
+  function showAllTransactions() {
+    var component = <p></p>
+    for (const transaction in transactions) {
+      component = <p>
+        {component}
+        {JSON.stringify(transactions[transaction])}
+      </p>
+    }
+    return Component;
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          we are that team!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
         <button onClick={activateLasers}>
           Activate Lasers
         </button>
 
-        <p>
-          we are that team!!!
-        </p>
-
         <p> {transactions.map((transaction, i) => {
           if (transaction.hasOwnProperty('Julia')) {
-            return (<p key={i}> Julia has: {SON.stringify(transaction.Julia)}</p>)
+            return (<p key={i}> Julia has: {JSON.stringify(transaction.Julia)}</p>)
           }
           return <p key={i}>{JSON.stringify(transaction)}</p>
         })}
@@ -47,6 +48,7 @@ function App() {
       </header>
     </div>
   );
+  
 }
 
 export default App;
